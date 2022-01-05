4dbd4def8f2c1de389715980cd1906fc3571d2cc
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-01-04 22:04:56 -0300

    get mainnet and polygon NFTs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
 
-const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
-const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+const APP_ID = "Z5GwmH1MgjdT8McD8Rv7I3F9Sb5ROjX4TsNrUD82"
+const SERVER_URL = "https://qzihuhp2fv72.usemoralis.com:2053/server"
 
 ReactDOM.render(
   // <React.StrictMode>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
-import { useMoralis } from "react-moralis";
+import Moralis from "moralis";
+import { useState } from "react";
 
 export function Home(){
 
-    const { authenticate, isAuthenticated, account, chainId, logout } = useMoralis();
+    const [currentUserAddress, setCurrentUserAddress] = useState()
+    const [mainnetNfts, setMainnetNfts] = useState()
+    const [polygonNFTs, setPolygonNFTs] = useState()
 
     async function handleAuth(){
-        await authenticate()
+        await Moralis.Web3.authenticate().then(async (user) => {
+
+            setCurrentUserAddress(user.get("ethAddress"))
+            
+            setMainnetNfts(await Moralis.Web3.getNFTs())
+            
+            setPolygonNFTs(await Moralis.Web3.getNFTs({chain: "matic", address: currentUserAddress}))
+
+            console.log(mainnetNfts)
+            console.log(polygonNFTs)
+        })
+
     }
 
     return(
